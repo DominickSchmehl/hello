3fb97cfe6010151a34228e881e340fe9b38e6869
DominickSchmehl <dschmehl2@unl.edu>
2024-08-29 09:51:42 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Dominick Schmehl
+ *         dschmehl2@huskers.unl.edu
+ * Date: 8/27/2024
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Dominick Schmehl, Electrical Engineer/n");
+  printf("I love the School of Computing/n");
+
+  return 0;
+}
